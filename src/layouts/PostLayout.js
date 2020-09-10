ab8a7e69191758179b0027ba577ec16d56b30229
Charles Buxton <chazbux@gmail.com>
2020-09-10 14:10:31 -0600

    seo fix

--- src/layouts/PostLayout.js
+++ src/layouts/PostLayout.js
@@ -2,11 +2,19 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import Header from '../components/header'
 import Footer from '../components/footer'
+import SEO from '../components/seo'
+import innertext from 'innertext'
 
 const PostLayout = ({data}) => {
   const post = data.wordpressPost
   return (
   <>
+  <SEO 
+    title={post.title} 
+    description={innertext(post.excerpt)} 
+    image={post.featured_media.source_url} 
+    keywords={post.categories.map(result => result.name).join(', ')} 
+    />
   <Header />
   <div className="container">
     <div className="row justify-content-md-center">
@@ -24,8 +32,13 @@ export default PostLayout
 export const query = graphql`
   query($slug: String!) {
     wordpressPost(slug: {eq:$slug}){
-        title
-        content
+      title
+      content
+      featured_media {
+        source_url
+      }
+      categories 
+      excerpt
     }
   }
 `
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,16 @@ import React from 'react'
 import Post from '../components/post'
 import PrimaryLayout from '../layouts/PrimaryLayout'
 import { graphql } from 'gatsby'
-import { Helmet } from 'react-helmet'
-// import Image from "../components/image"
-// import SEO from "../components/seo"
+import SEO from '../components/seo'
+
 
 export default (props) => {
   console.log(props.data)
 
   return (
     <>
+    <SEO/>
       <PrimaryLayout grid="col-xs-6">
-   
         {props.data.allWordpressPost.nodes.map(node => (
           <Post 
             title={node.title} 
